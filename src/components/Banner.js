af8b8d8ca9a56e02b10f79465d7a64a3ba10fe1e
codekayn <vadim.poznakhivski@gmail.com>
2022-12-28 02:30:17 +0200

    my-portfolio start

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -46,18 +48,24 @@ export const Banner = () => {
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my Portfolio</span>
-            <h1>
-              {"Hi  I'm a beginner Front-End developer "}
-              <span className="wrap">{text}</span>
-              <p>
-                Всім привіт.Вивчаю Front-end вже 700 годин. Проживаю в Україні,
-                м.Камянець-Подільський.На даний час мені 17років.
-              </p>
-              <button onClick={() => console.log("connect")}>
-                Lets Connect <ArrowRightCircle size={25} />
-              </button>
-            </h1>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <h1
+                  className={
+                    isVisible ? "animated__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h3 className="tagline">Welcome to my Portfolio</h3>
+                  <br/>
+                  {"Hi  I'm a beginner Front-End developer "}
+                  <span className="wrap">{text}</span>
+                  <p>
+                    Всім привіт.Вивчаю Front-end вже 700 годин. Проживаю в
+                    Україні, м.Камянець-Подільський.На даний час мені 17років.
+                  </p>
+                </h1>
+              )}
+            </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={7}>
             <img src={headerImg} alt="Header Img" />
